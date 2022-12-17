d3f58e10aa878e069d6a2bd0356c02483b528e5c
Saadat Badgujar <saadatbadgujar@gmail.com>
2022-12-17 21:46:43 +0530

    Adding register functionality & bcrypt

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-const express = require("express");
+require("dotenv").config();
+require("./config/database").connect();
 
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const User = require("./model/user");
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello from auth system - LCO</h1>");
+});
+
+app.post("/register", async (req, res) => {
+  const { firstname, lastname, email, password } = req.body;
+
+  if (!(email && password && firstname && lastname)) {
+    res.status(400).send("All fields are required");
+  }
+  const existingUser = User.findOne({ email });
+
+  if (existingUser) {
+    res.status(401).send("User already exists");
+  }
+
+  const myEncPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    firstname,
+    lastname,
+    email: email.toLowerCase(),
+    password: myEncPassword,
+  });
 });
 
 module.exports = app;
